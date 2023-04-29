ae7f52bcd659a8d9e955cbc7b73d5d50e37fcc18
Shrestha-Dipesh <shrestha.dipesh1999@gmail.com>
2023-04-29 19:47:27 +0545

    Configured Hub class with dependencies

---
+++ SignalRChatRoom/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChatRoom.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessageToAll(string userName, string message)
+        {
+            await Clients.Caller.SendAsync("SendMessage", message);
+            await Clients.Others.SendAsync("ReceiveMessage", userName, message);
+        }
+    }
+}
