e54b6fe51b19048296f966cf6690d98aca3d54d2
Shrestha-Dipesh <shrestha.dipesh1999@gmail.com>
2023-05-01 12:09:25 +0545

    Improved private message system (send message using username instead of connection id)

--- SignalRChatRoom/Hubs/ChatHub.cs
+++ SignalRChatRoom/Hubs/ChatHub.cs
@@ -4,6 +4,8 @@ namespace SignalRChatRoom.Hubs
 {
     public class ChatHub : Hub
     {
+        private static Dictionary<string, string> activeUsers = new Dictionary<string, string>();
+
         public async Task SendMessageToAll(string userName, string message)
         {
             await Clients.Caller.SendAsync("SendMessage", message);
@@ -14,6 +16,16 @@ namespace SignalRChatRoom.Hubs
         {
             await Clients.Caller.SendAsync("SendMessage", message);
             await Clients.Client(connectionId).SendAsync("ReceiveMessage", userName, message);
+        }
+
+        public void AddUser(string userName, string connectionId)
+        {
+            activeUsers.Add(userName, connectionId);
+        }
+
+        public Dictionary<string, string> GetActiveUsers()
+        {
+            return activeUsers;
         }
 
         public string GetConnectionId()
--- SignalRChatRoom/wwwroot/js/chat.js
+++ SignalRChatRoom/wwwroot/js/chat.js
@@ -5,28 +5,51 @@ const userName = document.querySelector('#userName');
 const message = document.querySelector('#message');
 const sendButton = document.querySelector('#sendButton');
 const messageList = document.querySelector('#messagesList');
-const connectionId = document.querySelector('#connectionId');
-const receiverId = document.querySelector('#receiverId');
+const receiverName = document.querySelector('#receiverName');
+const setUserName = document.querySelector('#setUserName');
+var connectionId;
 
 sendButton.disabled = true;
 connection.start().then(() => {
     sendButton.disabled = false;
     connection.invoke("GetConnectionId").then((id) => {
-        console.log(connectionId);
-        connectionId.textContent = `Your connection id: ${id}`;
+        connectionId = id;
     });
 }).catch((error) => {
     return console.error(error.toString());
 });
 
 sendButton.onclick = (event) => {
-    if (receiverId.value == null || receiverId.value == '') {
+    if (receiverName.value == null || receiverName.value == '') {
         connection.invoke("SendMessageToAll", userName.value, message.value)
             .catch((error) => {
                 return console.error(error.toString());
             });
     } else {
-        connection.invoke("SendMessageToUser", userName.value, receiverId.value, message.value)
+        connection.invoke("GetActiveUsers").then((list) => {
+            if (list != null || list.count > 0) {
+                for (var user in list) {
+                    if (user == receiverName.value) {
+                        var receiverId = list[user];
+                        connection.invoke("SendMessageToUser", userName.value, receiverId, message.value)
+                            .catch((error) => {
+                                return console.error(error.toString());
+                            });
+                        break;
+                    }
+                }
+            }
+        });
+        
+    }
+    event.preventDefault();
+};
+
+setUserName.onclick = (event) => {
+    if (userName.value.trim() != '') {
+        userName.disabled = true;
+        setUserName.remove();
+        connection.invoke("AddUser", userName.value, connectionId)
             .catch((error) => {
                 return console.error(error.toString());
             });
